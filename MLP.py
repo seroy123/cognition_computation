80672cba7241217d7fec7a5f91ea796a68ac5e91
Rotem Vekslar <rotemve@post.bgu.ac.il>
2022-04-25 18:47:35 +0300

    change to be only on-line learning

--- MLP.py
+++ MLP.py
@@ -11,78 +11,73 @@ class MLP:
         self.layer_num = layer_num
         self.active_func = active_func
         if len(self.active_func) != len(self.layer_num):
-            raise("number of activation functions must equal to number of layers")
+            raise ("number of activation functions must equal to number of layers")
         self.weights = []
         self.bias = []
 
     def weight_initialization(self):
         # TODO: add an option for a different method (f.e. xavier)
         # TODO: look where yonatan explained how to do it
-        self.bias = [np.random.randn(layer,1) for layer in self.layer_num]
+        self.bias = [np.random.randn(layer, 1) for layer in self.layer_num]
         for layer, next_layer in zip(self.layer_num[:-1], self.layer_num[1:]):
             self.weights += [np.random.randn(layer, next_layer)]
 
-    def gradient_descent(self, batch_data: np.ndarray, batch_labels: np.ndarray):
+    def gradient_descent(self, data: np.ndarray, label: np.ndarray):
         # TODO: maybe initialize weights here so you will have input size and make a field with bias initialization method
         # ΔW=-ηδ(w)*s.T
-        Delta_B= [np.zeros(b.shape) for b in self.bias]
+        Delta_B = [np.zeros(b.shape) for b in self.bias]
         Delta_W = [np.zeros(W.shape) for W in self.weights]
-        #TODO: make train data the size we want(batch)
-        # batch_size = len(batch_labels)
-        batch_size = 1
-        for ind in range(batch_size):
-            # x = batch_data[ind, :]
-            # y0 = batch_labels[ind]
-            x = batch_data
-            y0 = batch_labels
-            Z, A = self.forward_prop(x)
-            delta_B, delta_W = self.backward_prop(Z, A, x, y0)
-            # TODO: plaster... Delta of output should be row vector for adding the arrays
-            Delta_W[1] = Delta_W[1].T
-            Delta_B = [prev_Delta_B + current_Delta_B for prev_Delta_B, current_Delta_B in zip(Delta_B, delta_B)]
-            Delta_W = [prev_Delta_W + current_Delta_W for prev_Delta_W, current_Delta_W in zip(Delta_W, delta_W)]
+        x = data
+        y0 = label
+        Z, A = self.forward_prop(x)
+        delta_B, delta_W = self.backward_prop(Z, A, x, y0)
         Delta_W[1] = Delta_W[1].T
-        self.weights = [weight - (1 / batch_size) * current_Delta_W for weight, current_Delta_W in
+        Delta_B = [prev_Delta_B + current_Delta_B for prev_Delta_B, current_Delta_B in zip(Delta_B, delta_B)]
+        Delta_W = [prev_Delta_W + current_Delta_W for prev_Delta_W, current_Delta_W in zip(Delta_W, delta_W)]
+        Delta_W[1] = Delta_W[1].T
+        self.weights = [weight - current_Delta_W for weight, current_Delta_W in
                         zip(self.weights, Delta_W)]
-        self.bias = [bias - (1 / batch_size) * current_Delta_B for bias, current_Delta_B in
+        self.bias = [bias - current_Delta_B for bias, current_Delta_B in
                      zip(self.bias, Delta_B)]
 
-
     def forward_prop(self, X):
-        A = []
-        Z = []
+        activation_func_ans = []
+        weights_and_input_multiplication = []
         for bias, weight, current_sigma in zip(self.bias, self.weights, self.active_func):
-            current_Z = weight.T @ current_A + bias[0] if Z else weight.T @ X + bias[0]
+            current_weights_and_input_multiplication = weight.T @ current_activation_func_ans + bias[
+                0] if weights_and_input_multiplication else weight.T @ X + bias[0]
             # current_A = current_sigma(current_Z)
-            current_A = self.ReLU(current_Z)
-            Z.append(current_Z)
-            A.append(current_A)
-        return Z, A
+            current_activation_func_ans = self.ReLU(current_weights_and_input_multiplication)
+            weights_and_input_multiplication.append(current_weights_and_input_multiplication)
+            activation_func_ans.append(current_activation_func_ans)
+        return weights_and_input_multiplication, activation_func_ans
 
-    def backward_prop(self, Z, A, X, Y):
-        #TODO: create func_derivative method
-        hidden_layers_number = len(self.layer_num)-2  # removing input and output layer
+    def backward_prop(self, weights_and_input_multiplication, activation_func_ans, X, Y):
+        # TODO: create func_derivative method
+        hidden_layers_number = len(self.layer_num) - 2  # removing input and output layer
         delta_W = [np.zeros(W.shape) for W in self.weights]
         delta_B = [np.zeros(B.shape) for B in self.bias]
-        for layer in range(hidden_layers_number,-1,-1):
+        for layer in range(hidden_layers_number, -1, -1):
             # delta = func_derivative()
-            delta = self.ReLU_deriv(Z[layer]) * (self.weights[layer+1] @ delta.T) if layer != hidden_layers_number else\
-                                                 (A[layer]-Y).T * self.ReLU_deriv(Z[layer])
+            delta = self.ReLU_deriv(weights_and_input_multiplication[layer]) * (
+                    self.weights[layer + 1] @ delta.T) if layer != hidden_layers_number else \
+                (activation_func_ans[layer] - Y).T * self.ReLU_deriv(weights_and_input_multiplication[layer])
             delta_B[layer] = delta
-            delta_W[layer] = np.ravel(A[layer - 1])[np.newaxis].T @ delta.T if layer != 0 else np.ravel(X)[np.newaxis].T @ delta[np.newaxis]
-
+            delta_W[layer] = np.ravel(activation_func_ans[layer - 1])[np.newaxis].T @ delta.T if layer != 0 else \
+            np.ravel(X)[
+                np.newaxis].T @ \
+            delta[np.newaxis]
         return delta_B, delta_W
 
     def train(self, epochs, training_data, labels):
         self.weight_initialization()
         for i in range(epochs):
             for ind in range(len(labels)):
-                X = training_data[ind,:]
+                X = training_data[ind, :]
                 Y = labels[ind]
-                #TODO: divide to batchs in a seprate function that will receive as input the batch size
                 self.gradient_descent(X, Y)
 
-    def ReLU(self,Z):
+    def ReLU(self, Z):
         return np.maximum(Z, 0)
 
     def ReLU_deriv(self, Z):
--- main.py
+++ main.py
@@ -2,14 +2,25 @@ import numpy as np
 import MLP
 
 
-def call_MLP(X,Y):
+def call_MLP(X, Y):
     mlp = MLP.MLP([2, 15, 1], [MLP.MLP.ReLU] * 3)
-    mlp.train(20,X,Y)
+    mlp.train(20, X, Y)
+    return mlp
+
+
+def get_classification(mlp, X):
+    ans = []
+    for example in X:
+        ans.append(mlp.forward_prop(example)[-1][-1][0])
+    return ans
+
+
 if __name__ == '__main__':
     with open("./DATA_TRAIN.csv") as file_name:
         array = np.loadtxt(file_name, delimiter=",")
 
-    X = array[:,[0,1]]
-    Y = array[:,2]
-    call_MLP(X, Y)
-
+    X = array[:, [0, 1]]
+    Y = array[:, 2]
+    mlp = call_MLP(X, Y)
+    ans = get_classification(mlp, X)
+    print('yayy')
